861cf3c6e6a207f4f328b56aa75325c217a3fb49
eleonoraSt <eas402me@gmail.com>
2026-07-20 00:54:57 +0300

    LCS

--- include/editDistance.h
+++ include/editDistance.h
@@ -31,4 +31,24 @@ size_t Hamming(const std::basic_string<charT>& str1, const std::basic_string<cha
     return distance;
 }
 
+template <class charT>
+size_t Lcs(const std::basic_string<charT>& str1, const std::basic_string<charT>& str2) {
+    Matrix<size_t> matrix(str1.size() + 1, str2.size() + 1);
+
+    size_t size1 = str1.size(), size2 = str2.size();
+    for (size_t row = 1; row <= size2; row++) {
+        for (size_t column = 1; column <= size1; column++) {
+            size_t subseq;
+            if (str1.at(column) == str2.at(row)) {
+                subseq = matrix.Get(row - 1, column - 1) + 1;
+            } else {
+                subseq = std::max(matrix.Get(row - 1, column), matrix.Get(row, column - 1));
+            }
+            matrix.Set(subseq, row, column);
+        }
+    }
+
+    return matrix.Get(size2, size1);
+}
+
 #endif // EDITDISTANCE_H
