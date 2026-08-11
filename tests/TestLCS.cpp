5dfd995c1d2359cba00715edbd3dd17a1eb0a597
eleonoraSt <eas402me@gmail.com>
2026-08-11 22:23:40 +0300

    TestLCS

--- tests/TestLCS.cpp
+++ tests/TestLCS.cpp
@@ -2,61 +2,54 @@
 
 #include "..\include\editDistance.h"
 
-class TestLevenstein: public QObject {
+class TestLCS: public QObject {
     Q_OBJECT
 private slots:
     void identical() {
         std::string str1 = "aaa", str2 = "aaa";
-        QCOMPARE(Levenstein(str1, str2), 0);
+        QCOMPARE(Lcs(str1, str2), 3);
         std::wstring str3 = L"Հայոց", str4 = L"Հայոց";
-        QCOMPARE(Levenstein(str3, str4), 0);
+        QCOMPARE(Lcs(str3, str4), 5);
         std::u16string str5 = u"汉字", str6 = u"汉字";
-        QCOMPARE(Levenstein(str5, str6), 0);
+        QCOMPARE(Lcs(str5, str6), 2);
     }
 
     void insertion() {
         std::string str1 = "aaa", str2 = "aabca";
-        QCOMPARE(Levenstein(str1, str2), 2);
+        QCOMPARE(Lcs(str1, str2), 2);
         std::wstring str3 = L"Հայոց", str4 = L"աՀայոց";
-        QCOMPARE(Levenstein(str3, str4), 1);
+        QCOMPARE(Lcs(str3, str4), 5);
         std::u16string str5 = u"汉字", str6 = u"汉字字";
-        QCOMPARE(Levenstein(str5, str6), 1);
+        QCOMPARE(Lcs(str5, str6), 2);
     }
 
     void deletion() {
-        std::string str1 = "abc", str2 = "b";
-        QCOMPARE(Levenstein(str1, str2), 2);
+        std::string str1 = "abcd", str2 = "bc";
+        QCOMPARE(Lcs(str1, str2), 2);
+        std::string str2_5 = "ad";
+        QCOMPARE(Lcs(str1, str2_5), 1);
         std::wstring str3 = L"Հայոց", str4 = L"Հայ";
-        QCOMPARE(Levenstein(str3, str4), 2);
+        QCOMPARE(Lcs(str3, str4), 3);
         std::u16string str5 = u"汉字", str6 = u"字";
-        QCOMPARE(Levenstein(str5, str6), 1);
+        QCOMPARE(Lcs(str5, str6), 1);
     }
 
     void substitution() {
-        std::string str1 = "aaa", str2 = "cba";
-        QCOMPARE(Levenstein(str1, str2), 2);
+        std::string str1 = "aaa", str2 = "aba";
+        QCOMPARE(Lcs(str1, str2), 1);
         std::wstring str3 = L"Հայոց", str4 = L"Հայո!";
-        QCOMPARE(Levenstein(str3, str4), 1);
+        QCOMPARE(Lcs(str3, str4), 4);
         std::u16string str5 = u"汉字", str6 = u"水字";
-        QCOMPARE(Levenstein(str5, str6), 1);
-    }
-
-    void transposition() {
-        std::string str1 = "abc", str2 = "acb";
-        QCOMPARE(Levenstein(str1, str2), 2);
-        std::wstring str3 = L"Հայոց", str4 = L"Հյաոց";
-        QCOMPARE(Levenstein(str3, str4), 2);
-        std::u16string str5 = u"汉字", str6 = u"字汉";
-        QCOMPARE(Levenstein(str5, str6), 2);
+        QCOMPARE(Lcs(str5, str6), 1);
     }
 
     void mixed() {
         std::wstring str1 = L"гибралтар", str2 = L"лабрадор";
-        QCOMPARE(Levenstein(str1, str2), 5);
+        QCOMPARE(Levenstein(str1, str2), 3);
         std::string str3 = "example", str4 = "xeomple";
-        QCOMPARE(Levenstein(str3, str4), 3);
+        QCOMPARE(Levenstein(str3, str4), 4);
     }
 };
 
-QTEST_MAIN(TestLevenstein)
-#include "TestLevenstein.moc"
+QTEST_MAIN(TestLCS)
+#include "TestLCS.moc"
