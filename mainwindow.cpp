eb2edf99c7f52f2439d77f2f8e5f3267c2048eef
eleonoraSt <eas402me@gmail.com>
2026-08-23 12:05:08 +0300

    slots for buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,36 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "include/editDistance.h"
+
+void MainWindow::MetricSlotWrapper(std::function<size_t(std::wstring, std::wstring)> func, QLabel* outputLabel) {
+    std::wstring str1 = ui->textEdit->toPlainText().toStdWString();
+    std::wstring str2 = ui->textEdit_2->toPlainText().toStdWString();
+    size_t distance = func(str1, str2);
+    QString output = QString("%1").arg(distance);
+    outputLabel->setText(output);
+}
+
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::Levenstein_slot() {
+    MetricSlotWrapper(Levenstein<wchar_t>, ui->LevensteinLabel);
+}
+
+void MainWindow::DamerauLevenstein_slot() {
+    MetricSlotWrapper(DamerauLevenstein<wchar_t>, ui->DamerauLevensteinLabel);
+}
+
+void MainWindow::LCS_slot() {
+    MetricSlotWrapper(Lcs<wchar_t>, ui->LCSLabel);
+}
+
+void MainWindow::Hamming_slot() {
+    MetricSlotWrapper(Hamming<wchar_t>, ui->HammingLabel);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+
+#include <functional>
+#include <string>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,7 +20,15 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
+public slots:
+    void Levenstein_slot();
+    void DamerauLevenstein_slot();
+    void LCS_slot();
+    void Hamming_slot();
+
 private:
     Ui::MainWindow *ui;
+
+    void MetricSlotWrapper(std::function<size_t(std::wstring, std::wstring)> func, QLabel* outputLabel);
 };
 #endif // MAINWINDOW_H
