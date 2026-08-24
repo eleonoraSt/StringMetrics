984d91ff84c4b4eca22e12dc90b9fd07ec020955
eleonoraSt <eas402me@gmail.com>
2026-08-25 00:27:47 +0300

    rewrote LCS for variable length chars

--- include/editDistance.h
+++ include/editDistance.h
@@ -17,35 +17,36 @@ size_t DamerauLevenstein(const std::basic_string<charT>& str1, const std::basic_
 
 template <class charT>
 size_t Hamming(const std::basic_string<charT>& str1, const std::basic_string<charT>& str2) {
-    if (sizeInVarLengthChar(str1) != sizeInVarLengthChar(str2)) throw std::invalid_argument("Hamming distance not applicable");
+    size_t size1 = sizeInVarLengthChar(str1), size2 = sizeInVarLengthChar(str2);
+    if (size1 != size2) throw std::invalid_argument("Hamming distance not applicable");
     size_t distance = 0;
-    auto it1 = str1.begin();
-    auto it2 = str2.begin();
-    auto finish1 = str1.end();  // same number of non-continuation characters
-    while (it1 != finish1) {
-        if (isContinuation(*it1)) {
-            it1++;
+    size_t pos1 = 0, pos2 = 0;
+    while (pos1 < size1 && pos2 < size2) {
+        if (isContinuation(str1[pos1])) {
+            pos1++;
             continue;
         }
-        if (isContinuation(*it2)) {
-            it2++;
+        if (isContinuation(str2[pos2])) {
+            pos2++;
             continue;
         }
-        if (*it1 != *it2) distance++;
-        it1++;
-        it2++;
+        if (!equalChars(str1, pos1, str2, pos2)) distance++;
+        pos1++;
+        pos2++;
     }
     return distance;
 }
 
 template <class charT>
 size_t Lcs(const std::basic_string<charT>& str1, const std::basic_string<charT>& str2) {
-    size_t size1 = str1.size(), size2 = str2.size();
+    size_t size1 = sizeInVarLengthChar(str1), size2 = sizeInVarLengthChar(str2);
     std::vector<size_t> prevRow(size1 + 1), currentRow(size1 + 1);
 
     for (size_t row = 1; row <= size2; row++) {
+        if (isContinuation(str2[row])) continue;
         for (size_t column = 1; column <= size1; column++) {
-            if (str1[column - 1] == str2[row - 1]) {
+            if (isContinuation(str1[column])) continue;
+            if (equalChars(str1, column - 1, str2, row - 1)) {
                 currentRow[column] = prevRow[column - 1] + 1;
             } else {
                 currentRow[column] = std::max(prevRow[column], currentRow[column - 1]);
